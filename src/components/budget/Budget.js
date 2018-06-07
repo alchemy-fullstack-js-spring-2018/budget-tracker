5b7ca18945557479252cb07ee7a65f7188539655
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-07 11:34:11 -0700

    imported budget form

--- src/components/budget/Budget.js
+++ src/components/budget/Budget.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 // import { connect } from 'react-redux';
+import BudgetForm from './BudgetForm';
 
 class Budget extends Component {
 
   render() {
     return (
-      <h1>Hello World</h1>
+      <BudgetForm/>
     );
   }
 }
