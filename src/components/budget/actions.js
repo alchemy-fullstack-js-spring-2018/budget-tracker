bcc3070b3c9f323a71b353fac3296cc992766b59
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-07 10:16:29 -0700

    added budget array to the actions

--- src/components/budget/actions.js
+++ src/components/budget/actions.js
@@ -1,14 +1,21 @@
 import { BUDGET_LOAD, BUDGET_ADD, BUDGET_UPDATE, BUDGET_REMOVE } from './reducers';
 import shortid from 'shortid';
 
-const paycheck = {
+const budget = [
+  { 
     date: new Date('12/04/2018'),
     description: 'paycheck',
     amount: 500.34
-  };
-  
-  const rent = {
-    date: new Date('06/30/2018'),
+  },
+  {
+    date: new Date('12/30/2018'),
     description: 'rent',
     amount: -435.89
-  };
+  },
+  {
+    date: new Date('01/01/2019'),
+    description: 'bonus',
+    amount: 1000.00
+  }
+
+];
