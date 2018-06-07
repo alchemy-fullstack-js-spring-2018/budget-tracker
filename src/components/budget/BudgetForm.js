178401b22a603f87faf2146877305db29991b74a
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-07 11:41:15 -0700

    handle change

--- src/components/budget/BudgetForm.js
+++ src/components/budget/BudgetForm.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 
 const defaultState = {
-  date: null,
   description: '',
   amount: 0
 };
@@ -10,15 +9,19 @@ const defaultState = {
 export default class BudgetForm extends Component {
   state = defaultState;
 
+  handleChange = ({ target }) => {
+    this.setState({ [target.placeholder]: target.value });
+  };
+
   render() {
     const { date, description, amount } = this.state;
 
 
     return (
       <form>
-        <input placeholder="description" value={description}/>
-        <input placeholder="amount" value={amount}/>
-        <input placeholder="date" value={date}/>
+        <input placeholder="description" value={description} onChange={this.handleChange}/>
+        <input placeholder="amount" value={amount} onChange={this.handleChange}/>
+        <input placeholder="date" value={date} onChange={this.handleChange}/>
         <button type="submit" >Submit</button>
       </form>
     );
