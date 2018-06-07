e8584131e3fd82f8734f063f08aacd65057aa11c
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-07 11:06:23 -0700

    make it pass

--- src/components/reducers.js
+++ src/components/reducers.js
@@ -2,3 +2,10 @@ export const CATEGORIES_LOAD = 'CATEGORIES_LOAD';
 export const CATEGORY_ADD = 'CATEGORY_ADD';
 export const CATEGORY_UPDATE = 'CATEGORY_UPDATE';
 export const CATEGORY_REMOVE = 'CATEGORY_REMOVE';
+
+export function categories(state = [], {type, payload}) {
+  switch(type) {
+    default:
+      return state;
+  }
+}
