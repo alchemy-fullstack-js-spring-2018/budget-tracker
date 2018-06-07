9525d90707517e75b1bbd61aef0e1c4cf9cb1223
Ryan Manro <rmanro@gmail.com>
2018-06-06 22:57:55 -0700

    Added App.js

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Categories from '../categories/Categories';
+
+export default class App extends Component {
+  render() {
+    return (
+      <main>
+        <h1>Budget Tracker</h1>
+        <Categories/> {/*dashboard*/}
+      </main>
+    );
+  }
+}
