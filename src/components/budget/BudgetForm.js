25f91266f2caa0ffdb386c461ef934cf2b4e733a
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-07 11:37:31 -0700

    added default state

--- src/components/budget/BudgetForm.js
+++ src/components/budget/BudgetForm.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 
-// const defaultState = {
-//   date: null,
-//   description: '',
-//   amount: 0
-// };
+const defaultState = {
+  date: null,
+  description: '',
+  amount: 0
+};
 
 export default class BudgetForm extends Component {
+  state = defaultState;
+
   render() {
+    const { date, description, amount } = this.state;
+
+
     return (
       <form>
-        <input placeholder="description"/>
-        <input placeholder="amount"/>
-        <input placeholder="date"/>
-        <button>Submit</button>
+        <input placeholder="description" value={description}/>
+        <input placeholder="amount" value={amount}/>
+        <input placeholder="date" value={date}/>
+        <button type="submit" >Submit</button>
       </form>
     );
   }
