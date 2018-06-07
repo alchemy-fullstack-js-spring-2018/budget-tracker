d8b17a3d145e07e425b15ae31205561c9f593df5
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-07 09:32:05 -0700

    crated reducer

--- src/components/budget/reducers.js
+++ src/components/budget/reducers.js
@@ -3,3 +3,17 @@ export const BUDGET_ADD = 'BUDGET_ADD';
 export const BUDGET_UPDATE = 'BUDGET_UPDATE';
 export const BUDGET_REMOVE = 'BUDGET_REMOVE';
 
+export function budget(state = [], { type, payload }) {
+  switch (type) {
+    case BUDGET_LOAD:
+      return payload;
+    case BUDGET_ADD:
+      return [...state, payload];
+    case BUDGET_UPDATE:
+      return state.map(lineItem => lineItem.id === payload.id ? payload : lineItem);
+    case BUDGET_REMOVE:
+      return state.filter(lineItem => lineItem !== payload);
+    default:
+      return state;
+  }
+}
