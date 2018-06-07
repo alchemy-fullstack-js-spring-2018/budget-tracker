964dd7454b39df17fe84f3cffd4ecaf3e9e7297f
Ryan Manro <rmanro@gmail.com>
2018-06-06 22:28:51 -0700

    Add Action testing OK

--- src/components/categories/actions.test.js
+++ src/components/categories/actions.test.js
@@ -6,3 +6,15 @@ it('Creates a Load Action', () => {
   expect(type).toBe(CATEGORIES_LOAD);
   expect(payload.length).toBe(2);
 });
+
+it('Create and Add Action', () => {
+  const category = { name: 'Perfect Car', budget: 10000 };
+
+  const { type, payload } = addCategory(category);
+  expect(type).toBe(CATEGORY_ADD);
+  const { name, budget, id, timestamp } = payload;
+  expect(name).toBe(category.name);
+  expect(budget).toBe(category.budget);
+  expect(id).toBeTruthy();
+  expect(timestamp).toBeTruthy();
+});
