151f3cc2032175b7a90c07d742a53722e7c5f95b
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-07 10:55:37 -0700

    working on add action test

--- src/components/budget/actions.test.js
+++ src/components/budget/actions.test.js
@@ -1,8 +1,20 @@
 import { BUDGET_LOAD, BUDGET_ADD, BUDGET_UPDATE, BUDGET_REMOVE } from './reducers';
-import { loadBudget } from './actions';
+import { loadBudget, addLineItem } from './actions';
 
 it('creates a load action', () => {
   const { type, payload } = loadBudget();
   expect(type).toBe(BUDGET_LOAD);
   expect(payload.length).toBe(3);
+});
+
+it('creates a add action', () => {
+  const paycheck = { 
+    date: new Date('12/04/2018'),
+    description: 'paycheck',
+    amount: 500.34
+  };
+
+  const { type, payload } = addLineItem(paycheck);
+  expect(type).toBe(BUDGET_ADD);
+
 });
