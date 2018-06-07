9cb47452b3b5c22f0470b50c96e15a13e6b7c7f2
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-07 09:41:48 -0700

    created budget lineItems in the test

--- src/components/budget/reducer.test.js
+++ src/components/budget/reducer.test.js
@@ -9,3 +9,15 @@ it('has a default value of empty array', () => {
   const state = budget(undefined, {});
   expect(state).toEqual([]);
 });
+
+const paycheck = {
+  date: new Date('12/04/2018'),
+  description: 'paycheck',
+  amount: 500.34
+};
+
+const rent = {
+  date: new Date('06/30/2018'),
+  description: 'rent',
+  amount: 435.89
+};
