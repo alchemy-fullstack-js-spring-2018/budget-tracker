adae5cddcac0247fc684b2d31fe1dee86704b394
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-07 12:04:33 -0700

    added onSubmit

--- src/components/budget/BudgetForm.js
+++ src/components/budget/BudgetForm.js
@@ -13,12 +13,19 @@ export default class BudgetForm extends Component {
     this.setState({ [target.placeholder]: target.value });
   };
 
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+    // this.props.onComplete(this.state);
+    this.setState(defaultState);
+  };
+
   render() {
     const { date, description, amount } = this.state;
 
 
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <input placeholder="description" value={description} onChange={this.handleChange}/>
         <input placeholder="amount" value={amount} onChange={this.handleChange}/>
         <input placeholder="date" value={date} onChange={this.handleChange}/>
