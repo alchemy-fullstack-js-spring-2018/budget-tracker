21425910dcbfb146b35ac313ccc697190fd98fd0
jeffrey lonergan <jrlone1990@gmail.com>
2018-06-07 10:48:01 -0700

    recuser.test.1 failed

--- src/components/games/reducers.js
+++
@@ -1,7 +0,0 @@
-/*eslint-disable-next-line*/
-export function games(state = [], { type, payload }) {
-    switch (type) {
-        case GAMES_LOAD:
-            return payload;
-    }
-}
--- src/components/games/reducers.test.js
+++ src/components/games/reducers.test.js
@@ -1,9 +1,8 @@
 import {
-    games,
-    GAMES_LOAD
+  games
 } from './reducers';
 
-it('it has a default value of empty array', () => {
-    const state = games(undefined, {});
-    expect(state).toEqual([]);
-}); 
+it('has a default value of empty array', () => {
+  const state = games(undefined, {});
+  expect(state).toEqual([]);
+});
