0d6536ecb7632adf0d75c5652cd12a2243917a7c
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-07 11:23:54 -0700

    simple Budget component

---
+++ src/components/budget/Budget.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+// import { connect } from 'react-redux';
+
+class Budget extends Component {
+
+  render() {
+    return (
+      <h1>Hello World</h1>
+    );
+  }
+}
+
+export default Budget;
