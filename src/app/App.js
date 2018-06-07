61030209f50c18876e1e4daecd5d78c11dd0493c
Jen Lipton <jkl.lipton@gmail.com>
2018-06-06 17:45:21 -0700

    setup

---
+++ src/app/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Budget from '../budget/Budget';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <main>
+        <h1>Redux Demo</h1>
+        <Budget/>
+      </main>
+    );
+  }
+}
