64e9facd6dadca786a6349ac77f1bc5e482797ab
Steele Walston <steele.walston@gmail.com>
2018-06-07 09:38:26 -0700

    adds a category reducer  test passes

--- src/components/categories/reducers.test.js
+++ src/components/categories/reducers.test.js
@@ -10,3 +10,21 @@ it('has empty array as default', () => {
   expect(state).toEqual([]);
 });
 
+const budget1 = {
+  id: 1,
+  timstamp: '10/10/2018',
+  name: 'budget1',
+  budget: 3000,
+};
+
+const budget2 = {
+  id: 2,
+  timstamp: '10/10/2018',
+  name: 'budget2',
+  budget: 4000,
+};
+
+it('loads a category', () => {
+  const state = categories([], { type: CATEGORIES_LOAD, payload: [budget1, budget2]});
+  expect(state).toEqual([budget1, budget2]);
+});
