bf894ed1bb6c2b87265d9720040fe145f3b5d4bc
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-08 11:07:42 -0700

    updated budget in the actions

--- src/components/budget/actions.js
+++ src/components/budget/actions.js
@@ -1,27 +1,27 @@
 import { BUDGET_LOAD, BUDGET_ADD, BUDGET_UPDATE, BUDGET_REMOVE } from './reducers';
 import shortid from 'shortid';
 
-const budget = [
-  { 
+const budget = () => [
+  addLineItem({ 
     date: new Date('12/04/2018'),
     description: 'paycheck',
     amount: 500.34
-  },
-  {
+  }).payload,
+  addLineItem({
     date: new Date('12/30/2018'),
     description: 'rent',
     amount: -435.89
-  },
-  {
+  }).payload,
+  addLineItem({
     date: new Date('01/01/2019'),
     description: 'bonus',
     amount: 1000.00
-  }
+  }).payload
 ];
 
 export const loadBudget = () => ({
   type: BUDGET_LOAD,
-  payload: budget
+  payload: budget()
 });
 
 export const addLineItem = lineItem => {
