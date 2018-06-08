efc34bbbfa54ba698f12ef6ffc06072643c48025
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-08 10:48:06 -0700

    handling the update on the line item component

--- src/components/budget/LineItem.js
+++ src/components/budget/LineItem.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import BudgetForm from './BudgetForm';
 
 export default class LineItem extends Component {
 
@@ -13,17 +14,41 @@ export default class LineItem extends Component {
     onUpdate: PropTypes.func.isRequired
   };
 
+  handleEdit = () => {
+    this.setState({ editing: true });
+  };
+
+  handleCancel = () => {
+    this.setState({ editing: false });
+  };
+
+  handleUpdate = data => {
+    this.props.onUpdate(data);
+    this.setState({ editing: false });
+  };
     
   render() {
+    const { editing } = this.state;
     const { lineItem, onRemove } = this.props;
     const { description, amount, date } = lineItem;
 
     return (
-      <li key={description}>
+      <li key={description} style={{ backgroundColor: 'teal', color: 'white' }} >
         <h2>{description}</h2>
         <h2>${amount}</h2>
         <h2>{date.toLocaleString()}</h2>
-        <button onClick={() => onRemove(lineItem)}>delete</button>
+        {editing || <button onClick={this.handleEdit}>✐</button>}
+        <button onClick={() => onRemove(lineItem)}>X</button>
+        {editing && 
+          <div>
+            <BudgetForm
+              label="Update"
+              lineItem={lineItem}
+              onComplete={this.handleUpdate}
+              onCancel={this.handleCancel}
+            />
+          </div>
+        }
       </li>
     );
   }
