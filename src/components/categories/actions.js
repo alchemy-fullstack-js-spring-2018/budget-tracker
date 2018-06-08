97c74db4516190cf2a9f9c6d65cdf76d9fa0591c
Ryan Manro <rmanro@gmail.com>
2018-06-08 10:21:15 -0700

    EXPENSES_LOAD action testing ok

--- src/components/categories/actions.js
+++ src/components/categories/actions.js
@@ -1,9 +1,28 @@
 import { CATEGORIES_LOAD, CATEGORY_ADD, CATEGORY_REMOVE, CATEGORY_UPDATE } from './reducers';
+import { EXPENSES_LOAD, EXPENSES_CREATE, EXPENSES_UPDATE, EXPENSES_DELETE } from './reducers';
+
 import shortid from 'shortid';
 
 const categories = [
   { name: 'Perfect Vacation ', budget: 5000, id: shortid.generate(), timestamp: new Date() },
   { name: 'Perfect Home', budget: 100000, id: shortid.generate(), timestamp: new Date() }
+];
+
+const expenses = [
+  { 
+    id: shortid.generate(), 
+    categoryID: categories[0].id, 
+    timestamp: new Date(), 
+    name: 'Clothes',
+    price: 500
+  },
+  {
+    id: shortid.generate(), 
+    categoryID: categories[1].id, 
+    timestamp: new Date(), 
+    name: 'Furniture',
+    price: 2000
+  }
 ];
 
 export const loadCategories = () => ({ 
@@ -29,5 +48,10 @@ export const removeCategory = category => ({
 export const updateCategory = category => ({
   type: CATEGORY_UPDATE,
   payload: category
+});
+
+export const loadExpenses = () => ({
+  type: EXPENSES_LOAD,
+  payload: expenses
 });
 
--- src/components/categories/actions.test.js
+++ src/components/categories/actions.test.js
@@ -1,39 +1,56 @@
 import { CATEGORIES_LOAD, CATEGORY_ADD, CATEGORY_REMOVE, CATEGORY_UPDATE } from './reducers';
 import { loadCategories, addCategory, removeCategory, updateCategory } from './actions';
 
-it('Creates a Load Action', () => {
-  const { type, payload } = loadCategories();
-  expect(type).toBe(CATEGORIES_LOAD);
-  expect(payload.length).toBe(2);
-});
+import { EXPENSES_LOAD, EXPENSES_CREATE, EXPENSES_UPDATE, EXPENSES_DELETE } from './reducers';
+import { loadExpenses } from './actions';
 
-it('Create and Add Action', () => {
-  const category = { name: 'Perfect Car', budget: 10000 };
+describe('Categories Actions', () => {
 
-  const { type, payload } = addCategory(category);
-  expect(type).toBe(CATEGORY_ADD);
-  const { name, budget, id, timestamp } = payload;
-  expect(name).toBe(category.name);
-  expect(budget).toBe(category.budget);
-  expect(id).toBeTruthy();
-  expect(timestamp).toBeTruthy();
-});
-
-it('Create a Remove Action', () => {
-  const category = { name: 'Perfect Golf Swing', budget: 500 };
-
-  const action = removeCategory(category);
-  expect(action).toEqual({
-    type: CATEGORY_REMOVE,
-    payload: category
+  it('Creates a Load Action', () => {
+    const { type, payload } = loadCategories();
+    expect(type).toBe(CATEGORIES_LOAD);
+    expect(payload.length).toBe(2);
   });
+  
+  it('Create and Add Action', () => {
+    const category = { name: 'Perfect Car', budget: 10000 };
+  
+    const { type, payload } = addCategory(category);
+    expect(type).toBe(CATEGORY_ADD);
+    const { name, budget, id, timestamp } = payload;
+    expect(name).toBe(category.name);
+    expect(budget).toBe(category.budget);
+    expect(id).toBeTruthy();
+    expect(timestamp).toBeTruthy();
+  });
+  
+  it('Create a Remove Action', () => {
+    const category = { name: 'Perfect Golf Swing', budget: 500 };
+  
+    const action = removeCategory(category);
+    expect(action).toEqual({
+      type: CATEGORY_REMOVE,
+      payload: category
+    });
+  });
+  
+  it('Creat an Update Action', () => {
+    const category = { name: 'Perfect Dog', budget: 100 };
+    const action = updateCategory(category);
+    expect(action).toEqual({
+      type: CATEGORY_UPDATE,
+      payload: category
+    });
+  });
+
 });
 
-it('Creat an Update Action', () => {
-  const category = { name: 'Perfect Dog', budget: 100 };
-  const action = updateCategory(category);
-  expect(action).toEqual({
-    type: CATEGORY_UPDATE,
-    payload: category
+describe('Expenses Actions', () => {
+  
+  it('Creates a Load Action', () => {
+    const { type, payload } = loadExpenses();
+    expect(type).toBe(EXPENSES_LOAD);
+    expect(payload.length).toBe(2);
   });
+
 });
