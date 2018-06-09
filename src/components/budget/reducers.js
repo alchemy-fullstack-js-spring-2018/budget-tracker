0ad6fd8251bb58dfa300257da309577d01610cda
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-09 14:07:57 -0700

    commented out the budget from the reducer while I figure out the categories

--- src/components/budget/reducers.js
+++ src/components/budget/reducers.js
@@ -2,10 +2,10 @@ export const CATEGORIES_LOAD = 'CATEGORIES_LOAD';
 export const CATEGORY_ADD = 'CATEGORY_ADD';
 export const CATEGORY_UPDATE = 'CATEGORY_UPDATE';
 export const CATEGORY_REMOVE = 'CATEGORY_REMOVE';
-export const BUDGET_LOAD = 'BUDGET_LOAD';
-export const BUDGET_ADD = 'BUDGET_ADD';
-export const BUDGET_UPDATE = 'BUDGET_UPDATE';
-export const BUDGET_REMOVE = 'BUDGET_REMOVE';
+// export const BUDGET_LOAD = 'BUDGET_LOAD';
+// export const BUDGET_ADD = 'BUDGET_ADD';
+// export const BUDGET_UPDATE = 'BUDGET_UPDATE';
+// export const BUDGET_REMOVE = 'BUDGET_REMOVE';
 
 // export const getBudget = state => state.budget;
 // export const getCategory = state => state.budgetByCategory;
@@ -26,20 +26,20 @@ export function categories(state = [], { type, payload }) {
   }
 }
 
-export function budget(state = [], { type, payload }) {
-  switch (type) {
-    case BUDGET_LOAD:
-      return payload;
-    case BUDGET_ADD:
-      return [...state, payload];
-    case BUDGET_UPDATE:
-      return state.map(lineItem => lineItem.id === payload.id ? payload : lineItem);
-    case BUDGET_REMOVE:
-      return state.filter(lineItem => lineItem !== payload);
-    default:
-      return state;
-  }
-}
+// export function budget(state = [], { type, payload }) {
+//   switch (type) {
+//     case BUDGET_LOAD:
+//       return payload;
+//     case BUDGET_ADD:
+//       return [...state, payload];
+//     case BUDGET_UPDATE:
+//       return state.map(lineItem => lineItem.id === payload.id ? payload : lineItem);
+//     case BUDGET_REMOVE:
+//       return state.filter(lineItem => lineItem !== payload);
+//     default:
+//       return state;
+//   }
+// }
 
 // export function budgetByCategory(state = {}, { type, payload }) {
 //   switch (type) {
