af8d07ac510dbfc75ee174427f5a1da202fd6b1a
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-09 16:35:54 -0700

    cleaned up categories component

--- src/components/budget/Categories.js
+++ src/components/budget/Categories.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-// import BudgetForm from './BudgetForm';
 import Category from './Category';
 
 export default class Categories extends Component {
@@ -9,29 +8,22 @@ export default class Categories extends Component {
     categories: PropTypes.array,
     removeCategory: PropTypes.func.isRequired,
     updateCategory: PropTypes.func.isRequired
-
-    // loadBudget: PropTypes.func.isRequired,
-    // addLineItem: PropTypes.func.isRequired,
   };
 
   render() {
     const { categories, removeCategory, updateCategory } = this.props;
-    // if(!categories) return null;
 
     return (
-      <main>
-        {/* <BudgetForm onComplete={addLineItem} label="Add"/> */}
-        <ul>
-          <li>Hello World</li>
-          {categories.map(category => <Category 
-            key={category.name}
-            onRemove={removeCategory}
-            onUpdate={updateCategory}
-            category={category}
-          />)
-          }
-        </ul>
-      </main>
+      <ul>
+        <li>Categories</li>
+        {categories.map(category => <Category 
+          key={category.name}
+          onRemove={removeCategory}
+          onUpdate={updateCategory}
+          category={category}
+        />)
+        }
+      </ul>
     );
   }
 }
