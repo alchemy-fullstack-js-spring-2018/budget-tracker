0d7cb3ae6200cd5180cd50c1dfe5910e1b4cc012
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-10 11:37:34 -0700

    added categories to the budgetForm

--- src/components/budget/BudgetForm.js
+++ src/components/budget/BudgetForm.js
@@ -9,6 +9,7 @@ const defaultState = {
 
 export default class BudgetForm extends Component {
   static propTypes = {
+    categories: PropTypes.array,
     onComplete: PropTypes.func.isRequired,
     onCancel: PropTypes.func,
     label: PropTypes.string.isRequired
@@ -47,13 +48,20 @@ export default class BudgetForm extends Component {
 
   render() {
     const { date, description, amount } = this.state;
-    const { label, onCancel } = this.props;
+    const { label, onCancel, categories } = this.props;
+    if(!categories) return;
 
     return (
       <form onSubmit={this.handleSubmit}>
         <input placeholder="description" value={description} onChange={this.handleChange}/>
         <input placeholder="amount" value={amount} onChange={this.handleChange}/>
         <input type="date" placeholder="date" value={date} onChange={this.handleChange}/>
+        <select>
+          {categories.map(category => <option key={category.name} value={category.id}>
+            {category.name}
+          </option>)
+          }
+        </select>
         <button type="submit" >{label}</button>
         {onCancel && <button type="reset" onClick={onCancel}>Cancel</button>}
       </form>
