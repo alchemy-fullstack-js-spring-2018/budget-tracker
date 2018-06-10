54fd48d905c8934eb63ad296f6bda5d05f4dfe20
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-10 12:18:15 -0700

    setiting state on add

--- src/components/budget/LineItems.js
+++ src/components/budget/LineItems.js
@@ -11,17 +11,30 @@ class LineItems extends Component {
 
   static propTypes = {
     categories: PropTypes.array,
+    addLineItem: PropTypes.func
+  };
+
+  state = {
+    description: '',
+    amount: 0,
+    date: null
+  };
+
+  handleAdd = event => {
+    const { description, amount, date, categoryId } = event.edit;
+    this.setState({ description, amount, date });
+    
   };
 
 
   render() {
-    const { categories, addLineItem } = this.props;
+    const { categories } = this.props;
     if(!categories) return null;
 
     return (
       <div>
         <h2>Add a Line Item</h2>
-        <BudgetForm onComplete={addLineItem} categories={categories} label="Add"/>
+        <BudgetForm onComplete={this.handleAdd} categories={categories} label="Add"/>
         {/* <ul>
           {budget.map(lineItem => <LineItem 
             key={lineItem.description}
