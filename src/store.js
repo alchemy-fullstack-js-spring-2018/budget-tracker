034d2177b614ede07670399756af738f78eb9c8f
Katlyn Tucker <studioquirk@yahoo.com>
2018-06-11 10:02:54 -0700

    added store file

--- src/store.js
+++ src/store.js
@@ -1,17 +1,6 @@
 import { createStore, combineReducers } from 'redux';
 import { places } from './components/places/Place';
 
-//oops!  included accidentally.leaving for next part of lab
-// function reducer(state =[], { type }) {
-//   switch(type) {
-//     case 'INCREMENT':
-//       return state + 1;
-//     case 'DECREMENT':
-//       return state = -1;
-//     default:
-//       return state;
-//   }
-// }
 
 const rootReducer = combineReducers({ 
   places
