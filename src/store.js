42ae461a4f9d0709f46ff6802b332181e2969ee2
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-11 10:42:16 -0700

    updating store with error categories and middleware

--- src/store.js
+++ src/store.js
@@ -1,14 +1,29 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { categories, lineItemByCategory } from './components/budget/reducers';
+import { error, loading } from './components/app/reducers';
 
 const rootReducer = combineReducers({
+  error,
+  loading,
   categories,
   lineItemByCategory
 });
 
+const logger = () => next => action => {
+  console.log('redux logger received action:', action);
+  const result = next(action);
+  return result;
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware,
+    logger,
+    thunk
+  )
 );
 
 export default store;
