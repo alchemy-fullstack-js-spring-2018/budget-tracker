6a2bf7014de4a44db9150a1a699b86c2c06fc8f7
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-11 10:03:50 -0700

    setup for routes that are needed

---
+++ src/services/api.js
@@ -0,0 +1,21 @@
+const BASE_URL = 'https://open-budget-server.herokuapp.com/api/stephaniesmith/categories';
+
+export const getCategories = () => {
+
+};
+
+export const postCategory = category => {
+
+};
+
+export const putCategory = category => {
+
+};
+
+export const deleteCategory = id => {
+
+};
+
+export const postLineItem = (categoryId, lineItem) => {
+
+};
