9220ba22de4a1ac44e924f9022b6b6c0bb5fdcac
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-11 10:21:39 -0700

    setting up fetch calls to the api

--- src/services/api.js
+++ src/services/api.js
@@ -1,21 +1,43 @@
 const BASE_URL = 'https://open-budget-server.herokuapp.com/api/stephaniesmith/categories';
 
 export const getCategories = () => {
-
+  return fetch(`${BASE_URL}`, {
+    headers: {
+      'content-type': 'application/json'
+    },
+    method: 'GET'
+  });
 };
 
 export const postCategory = category => {
-
+  return fetch(`${BASE_URL}`, {
+    body: JSON.stringify(category),
+    headers: {
+      'content-type': 'application/json'
+    },
+    method: 'POST'
+  });
 };
 
 export const putCategory = category => {
-
+  return fetch(`${BASE_URL}/${category.id}`, {
+    body: JSON.stringify(category),
+    headers: {
+      'content-type': 'application/json'
+    },
+    method: 'PUT'
+  });
 };
 
 export const deleteCategory = id => {
-
+  return fetch(`${BASE_URL}/${id}`, {
+    method: 'DELETE'
+  });
 };
 
 export const postLineItem = (categoryId, lineItem) => {
-
+  return fetch(`${BASE_URL}/${categoryId}/lineItems`, {
+    body: JSON.stringify(lineItem),
+    method: 'POST'
+  });
 };
