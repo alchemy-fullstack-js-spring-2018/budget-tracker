b37ddb93d6c83bd77167756d993917cfba1182cc
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-11 06:16:08 -0700

    addExpense action creator passes test

--- src/components/actions.js
+++ src/components/actions.js
@@ -3,7 +3,10 @@ import {
   CATEGORIES_LOAD,
   CATEGORY_ADD,
   CATEGORY_UPDATE,
-  CATEGORY_REMOVE } from './reducers';
+  CATEGORY_REMOVE,
+  EXPENSE_CREATE,
+  EXPENSE_UPDATE,
+  EXPENSE_DELETE } from './reducers';
 
 const categories = [
   { id: '1', name: 'food', budget: 500 },
@@ -38,3 +41,13 @@ export const removeCategory = category => ({
   type: CATEGORY_REMOVE,
   payload: category
 });
+
+export const addExpense = (categoryId, expense) => {
+  expense.id = shortid.generate();
+  expense.timestamp = new Date();
+
+  return {
+    type: EXPENSE_CREATE,
+    payload: { categoryId, expense }
+  };
+};
--- src/components/actions.test.js
+++ src/components/actions.test.js
@@ -2,16 +2,18 @@ import {
   CATEGORIES_LOAD,
   CATEGORY_ADD,
   CATEGORY_UPDATE,
-  CATEGORY_REMOVE } from './reducers';
+  CATEGORY_REMOVE, 
+  EXPENSE_CREATE } from './reducers';
 
 import {
   loadCategories,
   addCategory,
   updateCategory,
-  removeCategory } from './actions';
+  removeCategory,
+  addExpense } from './actions';
 
-describe('categories', () => {
-  it('creates a load action', () => {
+describe('action creator', () => {
+  it('creates a load category action', () => {
     const { type, payload } = loadCategories();
     expect(type).toBe(CATEGORIES_LOAD);
     expect(payload).toHaveLength(3);
@@ -19,7 +21,7 @@ describe('categories', () => {
 
   let food = { id: 1, name: 'food', budget: 500 };  
 
-  it('creates an add action, which adds a timestamp and id', () => {
+  it('creates an add category action, which adds a timestamp and id', () => {
     const { type, payload } = addCategory(food);
     expect(type).toBe(CATEGORY_ADD);
     const { name, budget, id, timestamp } = payload;
@@ -29,15 +31,32 @@ describe('categories', () => {
     expect(timestamp).toBeTruthy();
   });
 
-  it('creates an update action', () => {
+  it('creates an update category action', () => {
     const { type, payload } = updateCategory(food);
     expect(type).toBe(CATEGORY_UPDATE);
     expect(payload).toEqual(food);
   });
 
-  it('creates a remove action', () => {
+  it('creates a remove category action', () => {
     const { type, payload } = removeCategory(food);
     expect(type).toBe(CATEGORY_REMOVE);
     expect(payload).toEqual(food);
+  });
+
+  it('creates an expense add action, which adds a timestamp and id', () => {
+    const parentId = '123';
+    const expenseData = { name: 'pizza', price: 4 };
+
+    const { type, payload } = addExpense(parentId, expenseData);
+    expect(type).toBe(EXPENSE_CREATE);
+
+    const { categoryId, expense } = payload;
+    expect(categoryId).toBe(parentId);
+
+    const { name, price, id, timestamp } = expense;
+    expect(name).toBe(expenseData.name);
+    expect(price).toBe(expenseData.price);
+    expect(id).toBeTruthy();
+    expect(timestamp).toBeTruthy();
   });
 });
