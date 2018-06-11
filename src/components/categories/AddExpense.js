799f4b4c09fa4bd2a3759fbd664f843e084723d3
victorbofill <victorm.bofill@gmail.com>
2018-06-11 11:23:04 -0700

    created expense file

---
+++ src/components/categories/AddExpense.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Expense extends Component {
+
+  static propTypes = {
+    onAdd: PropTypes.func.isRequired
+  };
+
+  state = {
+    text: ''
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.onAdd(this.state.text);
+    this.setState({ text: '' });
+  };
+
+  handleChange = ({ target }) => {
+    this.setState({ text: target.value });
+  };
+
+  render() {
+    const { text } = this.state;
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Add an expense:
+          <input name="text" value={text} onChange={this.handleChange} />
+        </label>
+        <button type="submit">Add Expense</button>
+      </form>
+    );
+  }
+}
