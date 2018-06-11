6768f13ad5d8365c8632bd87d090cfb41b82b3b8
victorbofill <victorm.bofill@gmail.com>
2018-06-11 12:34:54 -0700

    slight changes to categories and category; still not working

--- src/components/categories/Category.js
+++ src/components/categories/Category.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Expenses from './Expenses';
 
 export default class Category extends Component {
+
+  state = {
+    editing: false
+  };
 
   static propTypes = {
     category: PropTypes.object,
@@ -14,8 +19,12 @@ export default class Category extends Component {
 
     return (
       <li key={name}>
-        {name}: ${budget}
-        <button style={{ color: 'black' }} onClick={() => onRemove(category)}>X</button>
+        <h4>
+          <span>{name}</span>
+          <span>{budget}</span>
+          <button onClick={() => onRemove(category)}>X</button>
+        </h4>
+        <Expenses categoryId={category.id}/>
       </li>
     );
   }
