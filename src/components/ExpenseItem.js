b77cb21d49aa47dbd47eb966a99fa04c489f008a
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-17 08:20:33 -0700

    fill in expense item component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ExpenseForm from './ExpenseForm';
+import styles from './ExpenseItem.css';
 
 export default class ExpenseItem extends Component {
+  state = {
+    editing: false
+  };
+
   static propTypes = {
-    expense: PropTypes.object
+    expense: PropTypes.object,
+    onRemove: PropTypes.func.isRequired,
+    onUpdate: PropTypes.func.isRequired
+  };
+
+  handleEditToggle = () => {
+    this.setState(({ editing }) => ({ editing: !editing }));
+  };
+
+  handleUpdate = info => {
+    this.props.onUpdate(info);
+    this.setState({ editing: false });
   };
 
   render() {
-    const { name, price } = this.props.expense;
+    const { editing } = this.state;
+    const { expense, onRemove } = this.props;
+    const { name, price, timestamp } = expense;
+    const date = new Date(timestamp);
 
     return (
-      <li>
-        {name}: ${price}
-      </li>
+      <div className={styles['expense-item']}>
+        <li className={editing ? 'being-edited' : 'table-row'}>
+          <div className="cell">{name}</div>
+          <div className="cell">${price}</div>
+          <div className="cell">on {date.toLocaleDateString()}</div>
+          <div className="cell">
+            <button className="remove icon-button" onClick={onRemove}>&times;</button>
+            <button className={editing ? 'edit active' : 'edit icon-button'} onClick={this.handleEditToggle}>✎</button>  
+          </div>
+        </li>
+        {editing && <ExpenseForm name={name} price={price} onComplete={this.handleUpdate} label="UPDATE"/>} 
+      </div>
+
     );
   }
 }
